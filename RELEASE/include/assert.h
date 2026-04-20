ee9cb55ed46473885dfa2e7a7ac9513c0415e6c7
Reini Urban <reini.urban@gmail.com>
2026-04-20 12:01:42 +0200

    rm RELEASE artefacts
    this can be done with make dist easier

--- RELEASE/include/assert.h
+++
@@ -1,8 +0,0 @@
-#ifndef RCC_ASSERT_H
-#define RCC_ASSERT_H
-
-#include <stdlib.h>
-
-#define assert(expr) ((expr) ? (void)0 : abort())
-
-#endif
--- RELEASE/test/test_real.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("rcc is now a fully functioning compiler driver!\n");
-    return 0;
-}
