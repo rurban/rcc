013c83d3dc4203bfa4ecb949a9cd5bb3a93c9aaa
Reini Urban <reini.urban@gmail.com>
2026-05-31 02:29:30 -0500

    debug: fix seg_vmsize, add otool, restrict CI to macOS only

--- src/macho_write.c
+++ src/macho_write.c
@@ -295,9 +295,9 @@ int macho_write(ObjFile *obj, const char *path) {
     uint64_t strtab_off = symtab_off + symtab_size;
     uint32_t strtab_size = (uint32_t)mst.len;
 
-    // segment vm size = sum of all section sizes
-    uint64_t seg_vmsize = text_size + data_size + obj->bss_size + rodata_size;
-    // file size of segment = up to end of rodata
+    // segment vm size = from text_off to end of last section (including BSS padding)
+    uint64_t seg_vmsize = align(bss_off + obj->bss_size, 8) - text_off;
+    // file size of segment = up to end of rodata (BSS uses no file space)
     uint64_t seg_filesize = (rodata_off + rodata_size) - text_off;
 
     // -----------------------------------------------------------------------
